3233754854b4d54eaaddc854649a6737830ece29
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2024-03-25 16:08:32 +0900

    Added support for basic keyword management

--- index.js
+++ index.js
@@ -1,112 +1,127 @@
 // Keyword feature
-const fs = require('fs');
+import fs from "fs";
 
-function addKeyword(input) {
-    const [keyword, content] = input.split('::');
-    const newData = {
-        keyword: keyword.trim(),
-        content: content.trim()
-    };
-
-    fs.readFile('keywordDB.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err);
-            return;
-        }
-        try {
-            const keywordDB = JSON.parse(jsonString);
-            keywordDB.push(newData);
-            fs.writeFile('keywordDB.json', JSON.stringify(keywordDB, null, 2), 'utf8', (err) => {
-                if (err) {
-                    console.log("Error writing file:", err);
-                    return;
-                }
-				msg.replyText(newData.keyword + " has been added!");
-                console.log("Keyword content added successfully!");
-            });
-        } catch (err) {
-            console.log('Error parsing JSON string:', err);
-        }
-    });
+// Initialise keywordDB if it does not exist
+if (!fs.existsSync("./plugins/keyword-manager/keywordDB.json")) {
+	fs.writeFileSync("./plugins/keyword-manager/keywordDB.json", "{}", "utf8");
 }
 
-function editKeyword(input) {
-    const [keyword, content] = input.split('::');
-    const updatedData = {
-        keyword: keyword.trim(),
-        content: content.trim()
-    };
+// Load keywordDB from file
+let keywordDB = JSON.parse(
+	fs.readFileSync("./plugins/keyword-manager/keywordDB.json", "utf8")
+);
 
-    fs.readFile('keywordDB.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err);
-            return;
-        }
-        try {
-            let keywordDB = JSON.parse(jsonString);
-            const index = keywordDB.findIndex(item => item.keyword === keyword.trim());
-            if (index !== -1) {
-                keywordDB[index] = updatedData;
-                fs.writeFile('keywordDB.json', JSON.stringify(keywordDB, null, 2), 'utf8', (err) => {
-                    if (err) {
-                        console.log("Error writing file:", err);
-                        return;
-                    }
-				msg.replyText(updatedData.keyword + " has been updated!");
-                    console.log("Keyword content updated successfully!");
-                });
-            } else {
-                console.log("Keyword not found for editing.");
-            }
-        } catch (err) {
-            console.log('Error parsing JSON string:', err);
-        }
-    });
+export function processKeyword(msg) {
+	if (msg.content.startsWith("/add")) {
+		addKeyword(msg);
+	} else if (msg.content.startsWith("/edit")) {
+		editKeyword(msg);
+	} else if (msg.content.startsWith("/remove")) {
+		removeKeyword(msg);
+	} else if (msg.content.startsWith("/list")) {
+		listKeywords(msg);
+	} else {
+		// Echo stored keywords' contents
+		const keyword = msg.content;
+		if (keywordDB[keyword]) {
+			msg.replyText(keywordDB[keyword].content);
+		}
+	}
 }
 
-function removeKeyword(keyword) {
-    fs.readFile('keywordDB.json', 'utf8', (err, jsonString) => {
-        if (err) {
-            console.log("Error reading file from disk:", err);
-            return;
-        }
-        try {
-            let keywordDB = JSON.parse(jsonString);
-            const index = keywordDB.findIndex(item => item.keyword === keyword);
-            if (index !== -1) {
-                keywordDB.splice(index, 1);
-                fs.writeFile('keywordDB.json', JSON.stringify(keywordDB, null, 2), 'utf8', (err) => {
-                    if (err) {
-                        console.log("Error writing file:", err);
-                        return;
-                    }
-					msg.replyText(keyword + " has been removed!");
-                    console.log("Keyword removed successfully!");
-                });
-            } else {
-                console.log("Keyword not found for removal.");
-            }
-        } catch (err) {
-            console.log('Error parsing JSON string:', err);
-        }
-    });
+function addKeyword(msg) {
+	// msg.content = "/add newKeyword::New Content That Is Pretty Long::Category"
+	const keyword = msg.content.split("::")[0].split(" ")[1];
+	const content = msg.content.split("::")[1];
+	const category = msg.content.split("::")[2] || "unsorted";
+    const categoryLowerCase = category.toLowerCase();
+	if (!keywordDB[keyword]) {
+		keywordDB[keyword] = { content, category: categoryLowerCase };
+		fs.writeFileSync(
+			"./plugins/keyword-manager/keywordDB.json",
+			JSON.stringify(keywordDB, null, 2),
+			"utf8"
+		);
+		msg.replyText(`${keyword} has been added with category ${categoryLowerCase}`);
+	} else {
+		msg.replyText(`${keyword} already exists`);
+	}
+
+	// Reload keywordDB from file
+	keywordDB = JSON.parse(
+		fs.readFileSync("./plugins/keyword-manager/keywordDB.json", "utf8")
+	);
+}
+
+function editKeyword(msg) {
+	// msg.content = "/add newKeyword::New Content That Is Pretty Long::Category"
+	const keyword = msg.content.split("::")[0].split(" ")[1];
+	const content = msg.content.split("::")[1];
+	const category = msg.content.split("::")[2] || "unsorted";
+    const categoryLowerCase = category.toLowerCase();
+    console.log(categoryLowerCase);
+	if (keywordDB[keyword]) {
+		keywordDB[keyword] = { content, category: categoryLowerCase };
+		fs.writeFileSync(
+			"./plugins/keyword-manager/keywordDB.json",
+			JSON.stringify(keywordDB, null, 2),
+			"utf8"
+		);
+		msg.replyText(
+			`${keyword} has been updated with category ${categoryLowerCase}`
+		);
+	} else {
+		msg.replyText(`${keyword} does not exist`);
+	}
+
+	// Reload keywordDB from file
+	keywordDB = JSON.parse(
+		fs.readFileSync("./plugins/keyword-manager/keywordDB.json", "utf8")
+	);
+}
+
+function removeKeyword(msg) {
+	// msg.content = "/remove keyword"
+	const keyword = msg.content.split(" ")[1];
+	if (keywordDB[keyword]) {
+		delete keywordDB[keyword];
+		fs.writeFileSync(
+			"./plugins/keyword-manager/keywordDB.json",
+			JSON.stringify(keywordDB, null, 2),
+			"utf8"
+		);
+		msg.replyText(`${keyword} has been removed`);
+	} else {
+		msg.replyText(`${keyword} does not exist`);
+	}
+
+	// Reload keywordDB from file
+	keywordDB = JSON.parse(
+		fs.readFileSync("./plugins/keyword-manager/keywordDB.json", "utf8")
+	);
 }
 
 function listKeywords(msg) {
-    if (msg.content === "/keywords") {
-        fs.readFile('keywordDB.json', 'utf8', (err, jsonString) => {
-            if (err) {
-                console.log("Error reading file from disk:", err);
-                return;
-            }
-            try {
-                let keywordDB = JSON.parse(jsonString);
-                const keywordsList = keywordDB.map(item => item.keyword);
-                const replyMsg = "Keywords: " + keywordsList.join(", ");
-                msg.replyText(replyMsg);
-            } catch (err) {
-                console.log('Error parsing JSON string:', err);
-            }
-        });
-    }
+	// Sample JSON : { "keyword": { "content": "content", "category": "category" }
+	const keywordDB = JSON.parse(
+		fs.readFileSync("./plugins/keyword-manager/keywordDB.json", "utf8")
+	);
+	const categories = {};
+
+	Object.keys(keywordDB).forEach((keyword) => {
+		const { category } = keywordDB[keyword];
+		if (!categories[category]) {
+			categories[category] = [];
+		}
+		categories[category].push(keyword);
+	});
+
+	const sortedCategories = Object.keys(categories).sort(); // Sort categories alphabetically
+
+	let replyMessage = "Keyword List\n\n" + '\u200b'.repeat(500);
+	sortedCategories.forEach((category) => {
+		replyMessage += `# ${category.charAt(0).toUpperCase() + category.slice(1)} : \n${categories[category].join("\n")}\n\n`;
+	});
+
+	msg.replyText(replyMessage);
 }
